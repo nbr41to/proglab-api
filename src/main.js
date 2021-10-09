fae105e694f7fffa07e6407ef73e7af7f9f1270f
nbr41to <nbr.41to@gmail.com>
2021-10-09 17:28:27 +0900

    .

--- src/main.js
+++ src/main.js
@@ -18,54 +18,50 @@ const app = new App({
 // });
 
 /* Start Learning */
-app.shortcut(
-  'start_learning',
-  async ({ shortcut, ack, respond, client, body }) => {
-    await ack();
+app.shortcut('start_learning', async ({ ack, client, body }) => {
+  await ack();
 
-    try {
-      const result = await client.views.open({
-        // 適切な trigger_id を受け取ってから 3 秒以内に渡す
-        trigger_id: body.trigger_id,
-        view: {
-          type: 'modal',
-          callback_id: 'submit_learning_input',
-          title: {
-            type: 'plain_text',
-            text: 'オンライン自習室を開始します',
-          },
-          blocks: [
-            {
-              type: 'input',
-              block_id: 'text',
-              label: {
-                type: 'plain_text',
-                text: '意気込みをどうぞ！',
-              },
-              element: {
-                type: 'plain_text_input',
-                action_id: 'learning_input',
-                multiline: true,
-              },
-            },
-          ],
-          submit: {
-            type: 'plain_text',
-            text: 'Submit',
-          },
+  try {
+    await client.views.open({
+      // 適切な trigger_id を受け取ってから 3 秒以内に渡す
+      trigger_id: body.trigger_id,
+      view: {
+        type: 'modal',
+        callback_id: 'submit_learning_input',
+        title: {
+          type: 'plain_text',
+          text: 'オンライン自習室を開始します',
         },
-      });
-      console.log(result);
-    } catch (error) {
-      console.error(error);
-    }
+        blocks: [
+          {
+            type: 'input',
+            block_id: 'text',
+            label: {
+              type: 'plain_text',
+              text: '意気込みをどうぞ！',
+            },
+            element: {
+              type: 'plain_text_input',
+              action_id: 'learning_input',
+              multiline: true,
+            },
+          },
+        ],
+        submit: {
+          type: 'plain_text',
+          text: 'Submit',
+        },
+      },
+    });
+  } catch (error) {
+    console.error(error);
   }
-);
+});
 
 app.view('submit_learning_input', async ({ ack, body, view, client }) => {
   await ack();
 
-  console.log(view);
+  console.log('view: ', view);
 
   const text = `${view['state']['values']['text']['learning_input']['value']}
     <@${body.user.id}>
